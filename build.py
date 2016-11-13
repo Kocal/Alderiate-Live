5b9b0532225249b03896c7f436d89d5b43ecc7b4
Hugo Alliaume <kocal@live.fr>
2016-11-13 11:08:15 +0100

    Ajout d'un script pour buid plus facilement l'extension

---
+++ build.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import json
+import shutil
+import sys
+
+
+def help():
+    print('Usage: %s (chrome | firefox)' % sys.argv[0])
+    sys.exit(1)
+
+
+def extract_extension_version(browser):
+    extension_version = ''
+
+    if browser == 'chrome':
+        with open('chrome/manifest.json') as content:
+            content_json = json.load(content)
+            extension_version = content_json.get('version')
+    elif browser == 'firefox':
+        raise NotImplementedError()
+    else:
+        help()
+
+    return extension_version
+
+
+def main():
+    try:
+        browser = sys.argv[1]
+        extension_version = extract_extension_version(browser)
+        zip_name = 'dist/{}-{}'.format(browser, extension_version)
+
+        print('Building {}'.format(zip_name))
+
+        shutil.make_archive(zip_name, 'zip', browser)
+        print('Ok')
+    except IndexError:
+        help()
+
+
+if __name__ == '__main__':
+    main()
